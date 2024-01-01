dcad81fbcc9263c79741fa23ebf693d1c8385d90
Ayuh Juvekar <ayush@pop-os.localdomain>
2024-01-01 17:36:05 +0530

    Batman

---
+++ lua/plugins/conform.lua
@@ -0,0 +1,10 @@
+return {
+  "stevearc/conform.nvim",
+  opts = {
+    formatters_by_ft = {
+      ["java"] = { "google_java_format" },
+      ["html"] = { "prettier " },
+      ["css"] = { "prettier " },
+    },
+  },
+}
